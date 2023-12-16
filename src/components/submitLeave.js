c600a66b827e2a7d1fc670e00c598aced0093788
MADRIDBOI\17769 <@zhanghaotian7>
2023-12-16 09:50:30 +0800

    1.0

---
+++ src/components/submitLeave.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    DatePicker,
+    Form,
+    Input,
+    InputNumber,
+    Radio,
+    Select,
+    TreeSelect,
+} from 'antd';
+
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+const SubmitLeave = () => {
+    const [form] = Form.useForm();
+
+    const initialValues = {
+        LeaveType: 'lucy', // 设置 LeaveType 的初始值
+      };
+
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+    return (
+        <>
+            <Form
+                form={form}
+                name="submit"
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 14 }}
+                layout="horizontal"
+                style={{ maxWidth: 600 }}
+                onFinish={onFinish}
+                initialValues={initialValues}
+            >
+
+                <Form.Item label="LeaveType" name="LeaveType">
+                    <Select
+                        initialValues="lucy"
+                        style={{ width: 120 }}
+                        options={[
+                            { value: 'jack', label: 'Jack' },
+                            { value: 'lucy', label: 'Lucy' },
+                            { value: 'Yiminghe', label: 'yiminghe' },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item label="LeavePeriod" name="LeavePeriod">
+                    <RangePicker />
+                </Form.Item>
+                <Form.Item label="Dates" name="Dates">
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item label="Reasons" name="Reasons">
+                    <TextArea rows={4} />
+                </Form.Item>
+                <Form.Item label="ContactDetails" name="ContactDetails">
+                    <Input />
+                </Form.Item>
+                <Form.Item label=" ">
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
+    );
+};
+
+export default SubmitLeave;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,113 @@
+import React, { createContext, useContext, useState } from 'react';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+import SubmitLeave from '../../components/submitLeave';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [
+  { key: '1', icon: <UserOutlined />, label: 'Submit', component: () => <SubmitLeave /> },
+  { key: '2', icon: <VideoCameraOutlined />, label: 'Manage', component: () => <h1><h1>Component 2</h1>Component 2</h1> },
+  { key: '3', icon: <UploadOutlined />, label: 'View History', component: () => <h1>Component 3</h1> },
+  { key: '4', icon: <BarChartOutlined />, label: 'Nav 4', component: () => <h1>Component 4</h1> },
+  { key: '5', icon: <CloudOutlined />, label: 'Nav 5', component: () => <h1>Component 5</h1> },
+  { key: '6', icon: <AppstoreOutlined />, label: 'Nav 6', component: () => <h1>Component 6</h1> },
+  { key: '7', icon: <TeamOutlined />, label: 'Nav 7', component: () => <h1>Component 7</h1> },
+  { key: '8', icon: <ShopOutlined />, label: 'Nav 8', component: () => <h1>Component 8</h1> },
+];
+
+const NavContext = createContext();
+
+const NavProvider = ({ children }) => {
+  const [selectedNav, setSelectedNav] = useState('1');
+
+  return (
+    <NavContext.Provider value={{ selectedNav, setSelectedNav }}>
+      {children}
+    </NavContext.Provider>
+  );
+};
+
+const NavContent = () => {
+  const { selectedNav } = useContext(NavContext);
+
+  const selectedNavObj = items.find((item) => item.key === selectedNav);
+
+  return selectedNavObj.component();
+};
+
+const Home = () => {
+
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <NavProvider>
+      <Layout hasSider>
+        <Sider
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0,
+            top: 0,
+            bottom: 0,
+            flex: '0 0 200px'
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <NavMenu />
+        </Sider>
+        <Layout style={{ marginLeft: 200 }}>
+          <Header style={{ padding: 0, background: colorBgContainer, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <h1>Leave Application Processing System</h1>
+          </Header>
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial', position: 'relative' }}>
+            <div
+              style={{
+                padding: 24,
+                textAlign: 'center',
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <NavContent />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    </NavProvider>
+  );
+};
+
+const NavMenu = () => {
+  const { selectedNav, setSelectedNav } = useContext(NavContext);
+
+  const handleClick = ({ key }) => {
+    setSelectedNav(key);
+  };
+
+  return (
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={handleClick}>
+      {items.map((item) => (
+        <Menu.Item key={item.key} icon={item.icon}>
+          {item.label}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+};
+
+export default Home;
