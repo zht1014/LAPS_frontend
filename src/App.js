12b0d4f75f80e07d567942b51098b99d03fe73f1
NuClz <1742722856@qq.com>
2023-12-19 11:41:30 +0800

    add Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Home from './pages/Home'
+import Login from './pages/Login/Login'
 
 function App() {
   return (
@@ -8,8 +9,11 @@ function App() {
     <Router>
       <div className="App">
         <Link to="/home">首页</Link>
+        <br />
+        <Link to="/login">登录</Link>
         <Routes>
             <Route path="/home" element={<Home/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
         </Routes>
         
       </div>
