fd9f328424a3530adedefa7f6e477955031e51ad
zht <1776931339@qq.com>
2023-12-21 09:02:00 +0800

    history1

--- src/components/viewHistory.js
+++ src/components/viewHistory.js
@@ -1,35 +1,63 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import axios from "axios";
-import React from 'react';
+// import React from 'react';
 import { Card } from 'antd';
+import React, { useState, useEffect } from 'react';
 
 
 const ViewHistory = () => {
-    const list = [
-        {
-            fromDate: 2023 - 11 - 7,
-            leaveType: 'ANNUAL LEAVE'
-        },
-        {
-            fromDate: 2023 - 11 - 7,
-            leaveType: 'ANNUAL LEAVE'
-        }
-    ]
+    const [historyList, setHistoryList] = useState([]);
+    const user_id = 1; 
 
-    return (<Card title="Apllication History">
-        {
-            list.map((item) => {
-                return (<Card
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/application/get-application-by-employee-id/${user_id}`)
+            .then(response => {
+                const filteredData = response.data.map(item => ({
+                    fromDate: item.fromDate,
+                    leaveType: item.employeeLeaveType,
+                    dayOff: item.dayOff,
+                    applicationStatus: item.applicationStatus,
+                    compensationStart: item.compensationStart
+                }));
+                setHistoryList(filteredData);
+                console.log(filteredData)
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+            });
+    }, []); 
+
+
+    // const list = [
+    //     {
+    //         fromDate: 2023 - 11 - 7,
+    //         leaveType: 'ANNUAL LEAVE'
+    //     },
+    //     {
+    //         fromDate: 2023 - 11 - 7,
+    //         leaveType: 'ANNUAL LEAVE'
+    //     }
+    // ]
+
+    return (
+        <Card title="Application History">
+            {historyList.map((item, index) => (
+                <Card
+                    key={index}
                     style={{ marginTop: 16 }}
                     type="inner"
                     title={item.fromDate}
                     extra={<a href="#">More</a>}
                 >
-                    {item.leaveType}
-                </Card>)
-            })
-        }
-
-    </Card>)
+                    <p>Leave Type: {item.leaveType}</p>
+                    <p>Days Off: {item.dayOff}</p>
+                    <p>Application Status: {item.applicationStatus}</p>
+                    {item.compensationStart && (
+                        <p>Compensation Start: {item.compensationStart}</p>
+                    )}
+                </Card>
+            ))}
+        </Card>
+    );
 };
 export default ViewHistory
