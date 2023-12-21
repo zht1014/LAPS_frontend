809bee582e66085fc7bf413ae1163694a30b4b49
zht <1776931339@qq.com>
2023-12-21 09:38:52 +0800

    2.0

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,11 +19,11 @@ const items = [
   { key: '1', icon: <UserOutlined />, label: 'Submit', component: () => <SubmitLeave /> },
   { key: '2', icon: <VideoCameraOutlined />, label: 'Manage', component: () => <h1>Component 2</h1> },
   { key: '3', icon: <UploadOutlined />, label: 'View History', component: () => <ViewHistory /> },
-  { key: '4', icon: <BarChartOutlined />, label: 'Nav 4', component: () => <h1>Component 4</h1> },
+  /* { key: '4', icon: <BarChartOutlined />, label: 'Nav 4', component: () => <h1>Component 4</h1> },
   { key: '5', icon: <CloudOutlined />, label: 'Nav 5', component: () => <h1>Component 5</h1> },
   { key: '6', icon: <AppstoreOutlined />, label: 'Nav 6', component: () => <h1>Component 6</h1> },
   { key: '7', icon: <TeamOutlined />, label: 'Nav 7', component: () => <h1>Component 7</h1> },
-  { key: '8', icon: <ShopOutlined />, label: 'Nav 8', component: () => <h1>Component 8</h1> },
+  { key: '8', icon: <ShopOutlined />, label: 'Nav 8', component: () => <h1>Component 8</h1> }, */
 ];
 
 const NavContext = createContext();
