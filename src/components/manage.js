53b7d0a9b1863b0e337c658463cd837326493018
zht <1776931339@qq.com>
2023-12-24 01:14:32 +0800

    delete2

--- src/components/manage.js
+++ src/components/manage.js
@@ -5,30 +5,63 @@ import React, { useState, useEffect } from 'react';
 
 const Manage = () => {
     const [historyList, setHistoryList] = useState([]);
+    const user_id = 1;
     const list = [
         {
+            application_id: 1,
             fromDate: '2023 - 11 - 7',
             leaveType: 'ANNUAL LEAVE'
         },
         {
+            application_id: 2,
             fromDate: '2023 - 11 - 7',
             leaveType: 'ANNUAL LEAVE'
         }
-    ]
+    ] //测试时list也注释掉
+
+
     useEffect(() => {
-        setHistoryList(list)
-    }, [])
+        setHistoryList(list) //也注释掉
+        axios.get(`http://localhost:8080/api/application/get-application-by-employee-id/${user_id}`)
+            .then(response => {
+                const filteredData = response.data.map(item => ({
+                    application_id: item.application_id,
+                    fromDate: item.fromDate,
+                    leaveType: item.employeeLeaveType,
+                    dayOff: item.dayOff,
+                    applicationStatus: item.applicationStatus,
+                    compensationStart: item.compensationStart
+                }));
+                setHistoryList(filteredData);
+                console.log(filteredData)
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+            });
+    }, []);
 
     const confirm = (index) => {
-        const updatedList = [...historyList];
-        updatedList.splice(index, 1);
-        setHistoryList(updatedList);
-        message.success('Click on Yes');
+        const selected = historyList[index]
+        const application_id = selected.application_id
+        axios.delete(`localhost:8080/api/application/delete/${application_id}`)
+            .then(response => {
+                console.log('deleted!')
+                const updatedList = [...historyList];
+                updatedList.splice(index, 1);
+                setHistoryList(updatedList);
+                message.success('deleted!');
+            })
+            .catch(error => {
+                console.error("Error deleting data: ", error);
+                message.error("failed")
+
+            })
+
     };
 
     const cancel = (e) => {
         console.log(e);
-        message.error('Click on No');
+        message.error('cancelled');
     };
 
     return (
